e42a1bb6303ab20f45a9c4fc2d9020f5245a58ed
meenakshirana8587 <69763066+meenakshirana8587@users.noreply.github.com>
2021-03-24 01:32:25 -0700

    Extension method assignment

---
+++ Day8/Meenakshi/ConsoleApp/Extension.cs
@@ -0,0 +1,15 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+     static class Extension
+    {
+        public static void displayProduct(this Product p)
+        {
+            Console.WriteLine(p.Title + "\n " + p.price + " \n" + p.Color);
+        }
+    }
+}
