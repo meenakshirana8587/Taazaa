100548f2f2770c4bb225a506c39b827d40187e47
meenakshirana8587 <69763066+meenakshirana8587@users.noreply.github.com>
2021-03-25 18:19:56 -0700

    Polymorphism

---
+++ Day7/polymorphism/polymorphism/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymorphism
+{
+    class Person
+    {
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(int age, string name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        private string name;
+        public string _Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+
+
+
+
+
+        public  override  string ToString()
+        {
+             string s= Name + " is " + Age + " years old";
+            Console.WriteLine(s);
+            return s;
+        }
+
+        
+       
+        
+        
+    }
+}
---
+++ Day7/polymorphism/polymorphism/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Person p = new Person(15, "foo");
+            p.ToString();
+        }
+    }
+}
