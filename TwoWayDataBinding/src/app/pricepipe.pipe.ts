32eb56e48054abf3c2b7e51e77d85bea461d12c0
meenakshirana8587 <69763066+meenakshirana8587@users.noreply.github.com>
2021-04-06 22:41:51 +0530

    reactive forms

---
+++ TwoWayDataBinding/src/app/pricepipe.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IProduct } from './product/iproduct';
+
+@Pipe({
+  name: 'pricepipe'
+})
+export class PricepipePipe implements PipeTransform {
+
+  transform(value: IProduct[], args: number): IProduct[] {
+    if(!args)
+    {
+      return value;
+    }
+    return value.filter(item=>(item.Price>= args));
+  }
+
+}
